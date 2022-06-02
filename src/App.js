1d4f37a97c7021422b58bef26541ddd2311bf93a
Everson Jr <everson.c.junior@gmail.com>
2022-06-02 00:06:36 -0300

    show info basic fro  api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      memes: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((memes) =>
+        this.setState(() => {
+          return { memes: memes.data.memes };
+        })
+      );
+  }
+
+  prepareState(data, callback) {
+    this.setState(()=> {
+      return {memes: data}
+    },)
+  }
+
+  render() {
+    const memesFiltered = this.state.memes.filter((meme) => {
+      return meme.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+          placeholder="search memes"
+        />
+        {memesFiltered.map((meme) => {
+          return (
+            <div key={meme.id}>
+              <h1>{meme.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
